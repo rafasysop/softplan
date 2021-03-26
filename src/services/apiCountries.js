849932a57f8a22811ce149edcff4a3ffa929fd66
rafasysop <rafaelcostamoura@gmail.com>
2021-03-26 13:23:17 -0300

    Mudando estrutura para GraphQL

--- src/services/apiCountries.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = 'https://github.com/lennertVanSever/graphcountries';
-
-const fetchApi = async () => {
-  const response = await fetch(BASE_URL);
-  const result = await response.json()
-  return result;
-}
-
-export default fetchApi;
