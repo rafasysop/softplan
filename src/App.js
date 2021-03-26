9d20947ba084f3c9572eb52a5a7f8db118c0e548
rafasysop <rafaelcostamoura@gmail.com>
2021-03-26 13:56:32 -0300

    exibindo paises no Console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { client } from './config/client-graphql';
+import { gql } from '@apollo/client'
 
 function App() {
+  useEffect(() => {
+    initial();
+  }, [])
+
+  const initial = () => {
+    client.query({
+      query: gql`
+        query {
+          Country {
+            name
+            capital
+            flag {
+              emoji
+              emojiUnicode
+              svgFile
+            }
+          }
+        }
+      `
+    }).then(resp => console.log(resp.data))
+  }
+
   return (
     <div>
       App
